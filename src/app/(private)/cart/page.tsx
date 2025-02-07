e6f8b5728d1f2d8151207154873f8f9a5a5d05c4
deatiger <deatiger1492@gmail.com>
2025-02-07 14:12:48 +0900

    :necktie: Fix container / presentational pattern

--- src/app/(private)/cart/page.tsx
+++ src/app/(private)/cart/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next'
-import { Cart } from 'core/domain/entity/cart'
 import CartItemForm from './_components/CartItemForm'
 import { getCart } from './fetcher'
 
@@ -7,16 +6,8 @@ export const metadata: Metadata = {
   title: 'カート',
 }
 
-export default async function CartContainer() {
+export default async function CartPresentation() {
   const cart = await getCart()
-  return <CartPresentation cart={cart} />
-}
-
-type Props = {
-  cart: Cart
-}
-
-function CartPresentation({ cart }: Props) {
   return (
     <div>
       <h1>This is your cart!</h1>
--- src/app/(public)/post/_components/PostCards/index.tsx
+++ src/app/(public)/post/_components/PostCards/index.tsx
@@ -1,11 +1,11 @@
 import { Post } from 'core/domain/entity/post'
 import styles from './PostCard.module.scss'
 
-export default function PostCard({ post }: { post: Post }) {
-  return (
-    <div className={styles.card}>
+export default function PostCards({ posts }: { posts: Post[] }) {
+  return posts.map((post) => (
+    <div className={styles.card} key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
     </div>
-  )
+  ))
 }
