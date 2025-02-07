165799409dc23a637d82fbc750086c54f3ee3e20
deatiger <deatiger1492@gmail.com>
2025-02-07 17:46:21 +0900

    :bug: Fix import path

--- src/app/(public)/post/_components/PostCards/index.tsx
+++ src/app/(public)/post/_components/PostCards/index.tsx
@@ -1,5 +1,5 @@
 import { Post } from 'core/domain/entity/post'
-import styles from './PostCard.module.scss'
+import styles from './PostCards.module.scss'
 
 export default function PostCards({ posts }: { posts: Post[] }) {
   return posts.map((post) => (
